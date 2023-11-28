aa5fd8dbcfc27d9a166ed60420820bae34a2f388
hyewon218 <won_11111@naver.com>
2023-11-28 16:39:42 +0900

    Pessimistic Lock 활용

--- src/main/java/com/example/stockexample/repository/StockRepository.java
+++ src/main/java/com/example/stockexample/repository/StockRepository.java
@@ -2,7 +2,14 @@ package com.example.stockexample.repository;
 
 
 import com.example.stockexample.domain.Stock;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id=:id")
+    Stock findByIdWithPessimisticLock(Long id);
 }
--- src/test/java/com/example/stockexample/service/StockServiceTest.java
+++ src/test/java/com/example/stockexample/service/StockServiceTest.java
@@ -21,6 +21,9 @@ class StockServiceTest {
     private StockService stockService;
 
     @Autowired
+    private PessimisticLockStockService pessimisticLockStockService;
+
+    @Autowired
     private StockRepository stockRepository;
 
     @BeforeEach // 테스트 전 DB에 데이터 넣는 작업
@@ -62,6 +65,35 @@ class StockServiceTest {
             executorService.submit(() -> {
                 try {
                     stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (100 * 1) = 0
+        assertEquals(0L, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청_Pessimistic_Lock() throws Exception {
+        int threadCount = 100;
+
+        // ExecutorService : 비동기로 실행하는 작업을 단순하하여 사용할 수 있게 도와주는 자바의 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // CountDownLatch : 다른 스레드에서 수행중인 작업이 모두 완료될 때까지 대기할 수 있도록 도와주는 클래스
+        // 100개의 요청이 끝날때까지 기다려야하므로 CountDownLatch 를 사용
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    pessimisticLockStockService.decrease(1L, 1L);
                 } finally {
                     latch.countDown();
                 }
