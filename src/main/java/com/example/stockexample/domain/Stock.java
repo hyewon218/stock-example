a81e923c35b6b812863c8f77b6f0968b332652fe
hyewon218 <won_11111@naver.com>
2023-11-28 12:17:50 +0900

    재고감소 로직 작성

---
+++ src/main/java/com/example/stockexample/domain/Stock.java
@@ -0,0 +1,33 @@
+package com.example.stockexample.domain;
+
+import javax.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity < 0) {
+            throw new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+
+        this.quantity = this.quantity - quantity;
+    }
+}
---
+++ src/main/java/com/example/stockexample/service/StockService.java
@@ -0,0 +1,29 @@
+package com.example.stockexample.service;
+
+import com.example.stockexample.domain.Stock;
+import com.example.stockexample.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // Stock 조회
+        // 재고감소
+        // 저장
+
+        Stock stock = stockRepository.findById(id).orElseThrow();
+
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+}
