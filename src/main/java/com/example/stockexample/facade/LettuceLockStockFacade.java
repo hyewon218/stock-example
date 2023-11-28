49070a270f74e6ec311189000aef208ac5fec40b
hyewon218 <won_11111@naver.com>
2023-11-29 01:49:13 +0900

    Lettuce 활용

---
+++ src/main/java/com/example/stockexample/facade/LettuceLockStockFacade.java
@@ -0,0 +1,27 @@
+package com.example.stockexample.facade;
+
+import com.example.stockexample.repository.RedisLockRepository;
+import com.example.stockexample.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+
+    private final StockService stockService;
+
+    public void decrease(Long key, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(key)) { // lock 획득 실패 시
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(key, quantity);
+        } finally {
+            redisLockRepository.unlock(key);
+        }
+    }
+}
