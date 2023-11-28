1db167e48a2080b5054776028dc9a1ea4de25da2
hyewon218 <won_11111@naver.com>
2023-11-28 16:07:39 +0900

    Synchronized 이용

--- src/main/java/com/example/stockexample/service/StockService.java
+++ src/main/java/com/example/stockexample/service/StockService.java
@@ -3,7 +3,6 @@ package com.example.stockexample.service;
 import com.example.stockexample.domain.Stock;
 import com.example.stockexample.repository.StockRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -14,8 +13,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+    //@Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         // Stock 조회
         // 재고감소
         // 저장
