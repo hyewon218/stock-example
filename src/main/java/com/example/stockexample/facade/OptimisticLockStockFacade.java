81b127cae282df0797e8545df94063bd965883fd
hyewon218 <won_11111@naver.com>
2023-11-28 19:43:57 +0900

    Optimistic Lock 활용

---
+++ src/main/java/com/example/stockexample/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,27 @@
+package com.example.stockexample.facade;
+
+import com.example.stockexample.service.OptimisticLockStockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OptimisticLockStockFacade {
+
+    private final OptimisticLockStockService optimisticLockStockService;
+
+    @Transactional
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+
+        // update 실패 시 50ms 후 재실행하는 로직
+        while (true) {
+            try {
+                optimisticLockStockService.decrease(id, quantity);
+                break;
+            }catch (Exception e) {
+                Thread.sleep(50);
+            }
+        }
+    }
+}
